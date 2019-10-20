a275669f7d285368609ae6f2b8498f0f3dee5086
yarik.lysak <zhbrkscfr@gmail.com>
2019-10-20 19:31:51 +0300

    courses components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CourseComponent } from './components/courses-list/course/course.component';
+import { ManageCourseComponent } from './components/courses-list/manage-course/manage-course.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, LogoComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesListComponent,
+    CourseComponent,
+    ManageCourseComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
