424471269d25fa801fd3f329273ac66ba7028f1d
yarik.lysak <zhbrkscfr@gmail.com>
2019-10-20 19:53:05 +0300

    breadcrumbs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CourseComponent } from './components/courses-list/course/course.compone
 import { ManageCourseComponent } from './components/courses-list/manage-course/manage-course.component';
 import { LoginComponent } from './components/login/login.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
     ManageCourseComponent,
     LoginComponent,
     SearchBarComponent,
+    BreadcrumbsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
