71b323562e977723bdf0cc4806916d1014faa5ab
YarikLysak <zhbrkscfr@gmail.com>
2019-10-21 21:42:23 +0300

    First homework (#1)
    * create header component
    
    * create footer component
    
    * logo component
    
    * courses components
    
    * login component
    
    * create course and user interfaces and classes
    
    * search-bar component
    
    * breadcrumbs component
    
    * update components, add fontawesome cdn
    
    * fix types, lint errors and folder name for interfaces and entities, add pre-commit lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CourseComponent } from './components/courses-list/course/course.component';
+import { ManageCourseComponent } from './components/courses-list/manage-course/manage-course.component';
+import { LoginComponent } from './components/login/login.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesListComponent,
+    CourseComponent,
+    ManageCourseComponent,
+    LoginComponent,
+    SearchBarComponent,
+    BreadcrumbsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
