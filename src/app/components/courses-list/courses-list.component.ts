5882d83e57bad51d58f6802174403269f0c3f27b
YarikLysak <zhbrkscfr@gmail.com>
2019-10-28 15:47:04 +0200

    Update courses-list.component.ts

--- src/app/components/courses-list/courses-list.component.ts
+++ src/app/components/courses-list/courses-list.component.ts
@@ -10,22 +10,21 @@ export class CoursesListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    (() => {
-      for (let i = 0; i < 4; i++) {
-        this.courseList.push({
-          id: i,
-          title: `Video Course ${i}. Name tag`,
-          description: `
-          Learn about where you can find course descriptions, 
-          what information they include, 
-          how they work, and details about various components of a course description.
-          Course descriptions report information about a university or college's classes.
-          They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.`,
-          creationDate: '9 Nov.2018',
-          duration: '1h 55 min',
-        });
-      }
-    })();
+    for (let i = 0; i < 4; i++) {
+      this.courseList.push({
+        id: i,
+        title: `Video Course ${i}. Name tag`,
+        description: `
+        Learn about where you can find course descriptions, 
+        what information they include, 
+        how they work, and details about various components of a course description.
+        Course descriptions report information about a university or college's classes.
+        They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions 
+        for all courses offered during a particular semester.`,
+        creationDate: '9 Nov.2018',
+        duration: '1h 55 min',
+      });
+    }
   }
 
   onDelete(id: number) {
