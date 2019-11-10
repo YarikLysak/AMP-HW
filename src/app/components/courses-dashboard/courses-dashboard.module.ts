3361f29c3feb63688d6c0e67bc046ccf291a197c
YarikLysak <zhbrkscfr@gmail.com>
2019-11-10 14:55:25 +0200

    Third hw (#6)
    * breake into modules
    
    * cover by tests
    
    * change project structure and change styles
    
    * fix structure
    
    * hot fix

--- src/app/components/courses-dashboard/courses-dashboard.module.ts
+++ src/app/components/courses-dashboard/courses-dashboard.module.ts
@@ -6,9 +6,17 @@ import { RouterModule } from '@angular/router';
 import { CoursesListComponent } from './courses-list/courses-list.component';
 import { CourseComponent } from './course/course.component';
 import { ManageCourseComponent } from './manage-course/manage-course.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { LoadMoreComponent } from './load-more/load-more.component';
 
 @NgModule({
-  declarations: [CoursesListComponent, CourseComponent, ManageCourseComponent],
+  declarations: [
+    CoursesListComponent,
+    CourseComponent,
+    ManageCourseComponent,
+    SearchBarComponent,
+    LoadMoreComponent
+  ],
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
   exports: [CoursesListComponent, CourseComponent, ManageCourseComponent]
 })
